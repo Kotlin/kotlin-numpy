d19dc37c1b84acba8506f6aa4f397343dc795f0e
Pavel Gorgulov <devcrocod@gmail.com>
2020-04-01 15:59:55 +0300

    fix hyperbolic tests for linux

--- src/test/kotlin/mathFunctions/TestHyperbolic.kt
+++ src/test/kotlin/mathFunctions/TestHyperbolic.kt
@@ -4,6 +4,7 @@ import org.jetbrains.numkt.array
 import org.jetbrains.numkt.math.*
 import kotlin.math.E
 import kotlin.math.PI
+import kotlin.math.round
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -12,40 +13,41 @@ class TestHyperbolic {
     @Test
     fun testSinh() {
         val a = array(arrayOf(0))
-        assertEquals(sinh(a)[0], 0.0)
+        assertEquals(0.0, sinh(a)[0])
     }
 
     @Test
     fun testCosh() {
         val a = array(arrayOf(0))
-        assertEquals(cosh(a)[0], 1.0)
+        assertEquals(1.0, cosh(a)[0])
     }
 
     @Test
     fun testTanh() {
         val check = array(arrayOf(0.0, 0.9171523356672744, 0.99627207622075))
         val a = array(arrayOf(0.0, PI / 2, PI))
-        assertEquals(tanh(a), check)
+        assertEquals(check, tanh(a))
     }
 
     @Test
     fun testArcsinh() {
         val check = array(arrayOf(1.725382558852315, 2.99822295029797))
         val a = array(arrayOf(E, 10.0))
-        assertEquals(arcsinh(a), check)
+        assertEquals(check, arcsinh(a))
     }
 
     @Test
     fun testArccosh() {
         val check = array(arrayOf(1.6574544541530771, 2.993222846126381))
         val a = array(arrayOf(E, 10.0))
-        assertEquals(arccosh(a), check)
+        assertEquals(check, arccosh(a))
     }
 
     @Test
     fun testArctanh() {
-        val check = array(arrayOf(0.0, -0.5493061443340549))
+//        val check = array(arrayOf(0.0, -0.5493061443340549))
+        val check = array(arrayOf(0.0, -0.54931))
         val a = array(arrayOf(0.0, -0.5))
-        assertEquals(arctanh(a), check)
+        assertEquals(check, around(arctanh(a), 5))
     }
 }
